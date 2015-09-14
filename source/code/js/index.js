a29b661713edf2343eb2f8541ac0d364d200d8e2
iourt <iourt@github.com>
2015-09-14 10:00:32 +0800

    moidfy

--- source/code/js/index.js
+++ source/code/js/index.js
@@ -6,34 +6,36 @@ angular.module('Huijm')
     $rootScope.showHeader = true;
 
     $scope.chartConfig = {
-    chart: {
-    type: 'line'
-    },
-    title: {
-    text: '月平均浏览量'
-    },
-    subtitle: {
-    text: 'huijiame.com'
-    },
-    xAxis: {
-    categories: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
-    },
-    yAxis: {
-    title: {
-    text: '浏览值'
-    }
-    },
-    plotOptions: {
-    line: {
-    dataLabels: {
-    enabled: true
-    },
-    enableMouseTracking: false
-    }
-    },
-    series: [{
-    name: '日浏览数据',
-    data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-    }]
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: '月平均浏览量'
+        },
+        subtitle: {
+            text: 'huijiame.com'
+        },
+        xAxis: {
+            categories: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+        },
+        yAxis: {
+            title: {
+                text: '浏览值'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [
+            {
+                name: '日浏览数据',
+                data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            }
+        ]
     };
 });
