014ee2fba32b6c04182b42934c9fdc13e7d002ca
iourt <iourt@github.com>
2015-09-28 13:50:57 +0800

    modify

--- source/code/js/client.js
+++ source/code/js/client.js
@@ -12,30 +12,118 @@ Huijm
         TimeText: widget.getTimeText($stateParams.type)
     };
 
+    var colors = Highcharts.getOptions().colors,
+        categories = ['Android', 'IOS'],
+        data = [
+            {
+                y: 55.11,
+                color: colors[5],
+                drilldown: {
+                    name: 'Android versions',
+                    categories: ['Android 3.0', 'Android 4.0', 'Android 8.0', 'Android 9.0'],
+                    data: [10.85, 7.35, 33.06, 2.81],
+                    color: colors[0]
+                }
+            }, {
+                y: 21.63,
+                color: colors[6],
+                drilldown: {
+                    name: 'IOS versions',
+                    categories: ['IOS 2.0', 'IOS 3.0', 'IOS 3.5', 'IOS 3.6', 'IOS 4.0'],
+                    data: [0.20, 0.83, 1.58, 13.12, 5.43],
+                    color: colors[1]
+                }
+            }
+        ],
+        browserData = [],
+        versionsData = [],
+        i,
+        j,
+        dataLen = data.length,
+        drillDataLen,
+        brightness;
+
+
+    // Build the data arrays
+    for (i = 0; i < dataLen; i += 1) {
+
+        // add browser data
+        browserData.push({
+            name: categories[i],
+            y: data[i].y,
+            color: data[i].color
+        });
+
+        // add version data
+        drillDataLen = data[i].drilldown.data.length;
+        for (j = 0; j < drillDataLen; j += 1) {
+            brightness = 0.2 - (j / drillDataLen) / 5;
+            versionsData.push({
+                name: data[i].drilldown.categories[j],
+                y: data[i].drilldown.data[j],
+                color: Highcharts.Color(data[i].color).brighten(brightness).get()
+            });
+        }
+    }
+
 
     function charts() {
         var charts = new Highcharts.Chart({
             chart: {
                 renderTo : 'chart',
-                type: 'line'
+                type: 'pie'
             },
             title: {
-                text: $scope.Page.TimeText
-            },
-            xAxis: {
-                categories: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+                text: '客户端分布图',
+                align: 'left',
+                margin: 20,
+                style: {
+                    fontSize: '12px',
+                    color: '#999'
+                }
             },
             yAxis: {
                 title: {
-                    text: $scope.Page.CheckText
+                    text: 'Total percent market share'
                 }
             },
-            series: [
-                {
-                    name: $scope.Page.CheckText,
-                    data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            plotOptions: {
+                pie: {
+                    shadow: false,
+                    center: ['50%', '50%']
                 }
-            ]
+            },
+            tooltip: {
+                valueSuffix: '%'
+            },
+            series: [{
+                name: 'Browsers',
+                data: browserData,
+                size: '60%',
+                dataLabels: {
+                    formatter: function () {
+                        return this.y > 5 ? this.point.name : null;
+                    },
+                    color: 'white',
+                    distance: -30
+                }
+            }, {
+                name: 'Versions',
+                data: versionsData,
+                size: '80%',
+                innerSize: '60%',
+                dataLabels: {
+                    formatter: function () {
+                        // display only if larger than 1
+                        return this.y > 1 ? '<b>' + this.point.name + ':</b> ' + this.y + '%'  : null;
+                    }
+                }
+            }],
+            credits:{
+                 enabled: false
+            }
         });
     }
+
+    charts();
 });
